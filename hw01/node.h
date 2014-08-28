b2da6ade0328c686cb1d3090df14d405a19b13d2
Yingying Wu <wu_yingying@wheatoncollege.edu>
2014-08-28 04:01:37 -0400

    Create node.h

---
+++ hw01/node.h
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// node.h -- these are the NODES on the list
+#ifndef NODE
+#define NODE
+
+// NODE for ONE elephant
+typedef
+
+struct node{
+	string representative;
+	string year;
+	string party;
+	node *next;
+};
+
+#endif
